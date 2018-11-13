a8ec7abef64a72fb041d21d0f12efb72cbc7fd7f
Jayoung17 <young_1_5@naver.com>
2018-11-13 10:48:42 +0900

    my fourth commit

--- check.c
+++ check.c
@@ -3,31 +3,23 @@
 int is_valid(char *op, char *args)
 {
 	int i = 0;
+	
+	if(args[i] == NULL)
+		return 0;
+
 	while(args[i] != NULL) {
 		if(args[i] == ',')
 			break;
 		i++;
-	}
-	
-	if(args[0]=='0'||args[0]=='-'||args[0]=='(') {
-		if(args[i+1]=='0'||args[i+1]=='(')
+	}	
+
+	if(args[0] == '0' || args[i-1] == ')'){
+		if(args[i+1] == '0' || args[i+1] == '(')
 			return 0;
 	}
 
-
-	if(args[i+1]=='$')
+	if(args[i+1] == '$')
 		return 0;
 
-
-
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
 	return 1;
 }
