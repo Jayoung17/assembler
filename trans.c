c7a42c5d8a945df167b816628fafc211b5e64d26
Jayoung17 <young_1_5@naver.com>
2018-11-13 11:11:40 +0900

    my eighth commit

--- trans.c
+++ trans.c
@@ -2,7 +2,7 @@
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	int i;
+	int i = 0;
 	
 	// check syntax 
 	if(!is_valid(op, args)){
@@ -16,35 +16,36 @@ int instr_trans(char *op, char *args, char* mcode)
 	if(args[i] == NULL)
 		return 0;
 
-	else{
-		while(args[i] != NULL){
-			if(args[i] == ','){
-				// 1. reg to reg
-				if(args[0] == '%' && args[i+1] == '%')
-					strcpy(mcode, "89");
-				// 2,3. mem to reg
-				else if(args[i-1] == ')' && args[i+1] == '%')
-					strcpy(mcode, "8b");
-				// 4. mem to reg(eax)
-				else if(args[0] == '0' && args[i+1] == '%')
-					strcpy(mcode, "a1");
-				// 5. reg(eax) to mem
-				else if(args[0] == '%' && args[i+1] == '0')
-					strcpy(mcode, "a3");
-				// 6. immediate to reg
-				else if(args[0] == '$' && args[i+1] == '%'){
-					if(args[i+3] == 'a')
-						strcpy(mcode, "b8");
-					else if(args[i+3] == 'b')
-						strcpy(mcode, "bb");
-					else if(args[i+3] == 'c')
-						strcpy(mcode, "b9");
-					else if(args[i+3] == 'd')
-						strcpy(mcode, "ba");
-				}
+	while(args[i] != NULL){
+		if(args[i] == ','){
+			// 1. reg to reg
+			if(args[0] == '%' && args[i+1] == '%')
+				strcpy(mcode, "89");
+			// 2,3. mem to reg
+			else if(args[i-1] == ')' && args[i+1] == '%')
+				strcpy(mcode, "8b");
+			// 4. mem to reg(eax)
+			else if(args[0] == '0' && args[i+1] == '%')
+				strcpy(mcode, "a1");
+			// 5. reg(eax) to mem
+			else if(args[0] == '%' && args[i+1] == '0')
+				strcpy(mcode, "a3");
+			// 6. immediate to reg
+			else if(args[0] == '$' && args[i+1] == '%'){
+				if(args[i+3] == 'a')
+					strcpy(mcode, "b8");
+				else if(args[i+3] == 'b')
+					strcpy(mcode, "bb");
+				else if(args[i+3] == 'c')
+					strcpy(mcode, "b9");
+				else if(args[i+3] == 'd')
+					strcpy(mcode, "ba");
 			}
 		}
+
+		i++;
 	}
+	
 
 	return 1;	
 }
